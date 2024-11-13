4e438082857b362409fa98f93acf7614e899335e
Svansaen <renanrodriguespereira@gmail.com>
2024-11-13 18:38:10 -0300

    Add files via upload

---
+++ Aula 01-01.py
@@ -0,0 +1,6 @@
+n1 = 10
+n2 = 5.6
+nome = "Renan" 
+idade = 30
+print ("Sr(a)" , nome , ", A sua idade é" , idade , "anos")
+
---
+++ Aula 01-02.py
@@ -0,0 +1,3 @@
+num = int(input ( 'Informe um Valor:' ))
+result = num * 2
+print (" O resultado é:" , result)
