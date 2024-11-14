5147264de4833b913aaa4f75d097b958e48fe353
Svansaen <renanrodriguespereira@gmail.com>
2024-11-13 21:09:56 -0300

    Add files via upload

---
+++ Aula 02-05.py
@@ -0,0 +1,10 @@
+# Usando a biblioteca datetime
+import datetime
+print(datetime.date.today())
+dataatual = datetime.date.today()
+anoatual = dataatual.year
+print(anoatual)
+mesatual = dataatual.month
+print(mesatual)
+diaatual = dataatual.day
+print(diaatual)
