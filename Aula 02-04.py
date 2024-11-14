d3d2e3fb6c3080e9c9ef71886070e1535cb0a2a0
Svansaen <renanrodriguespereira@gmail.com>
2024-11-13 21:19:05 -0300

    Add files via upload

--- Aula 02-04.py
+++ Aula 02-04.py
@@ -1,5 +1,7 @@
 # Programa de calcular idade
+import datetime # Importa a biblioteca datetime
+data_atual = datetime.date.today() # Armazena na variavel a data completa
+ano_atual = data_atual.year # Armazena na variavel o ano atual
 nascimento = int(input("Informe o ano em que você nasceu: "))
-ano = int(input("Informe o ano atual: "))
-idade = (ano - nascimento )
-print(f"A sua idade é: {idade}")
+idade = (ano_atual- nascimento )
+print(f"A sua idade é: {idade} anos.")
